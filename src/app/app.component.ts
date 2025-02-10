a18cfeeccfdf6fb00b708e0d59eec8d2cb7872eb
VictorHOG95 <victorhog95@gmail.com>
2025-02-10 16:37:20 -0500

    Agregar un formulario a su aplicación Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, RouterModule],
+  //templateUrl: './app.component.html',
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="assets/img/angular.png"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PrimerProyecto';
